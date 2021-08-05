8a9b19f272c2e4c1612ab1c9f46baa0f7e2feaa0
Fabricio Nogueira <fabriciopinheiro713@gmail.com>
2021-08-05 09:37:32 -0300

    feat: creating task list component.

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="tasks">
+        <div v-for="task in tasks" :key="task.name"> {{task.name}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: { type: Array, required: true },
+  },
+};
+</script>
